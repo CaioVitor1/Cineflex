cfadebbffec1a7331a628b76b84ddde4d4866e25
CaioVitor1 <caiovitor33@hotmail.com>
2022-05-21 19:20:30 -0300

    finalizing requirement - seats choice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InitialPage from "./InitialPage"
 import Header from "./Header";
 import MovieTime from "./MovieTime";
 import MoviePlace from "./MoviePlace"
-//<Route path="/assentos/:idSessao" element={<MoviePlace />} />
+import Sucess from "./Sucess";
+
+
 export default function App(){
+    const[nomeComprador, setNomeComprador] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [idPlace, setIdPlace] = useState([]);
+    const [user, setUser] = useState({
+        ids: idPlace,
+        name: nomeComprador,
+        cpf: cpf
+    })
     
+
     return(
        <BrowserRouter>
             <Header />
             <Routes>
                 <Route path="/" element={<InitialPage />} />
                 <Route path="/sessoes/:idFilme" element={<MovieTime />} />
-                <Route path="/assentos/:idSessao" element={<MoviePlace />} />
+                <Route path="/assentos/:idSessao" setUser={setUser} user={user} element={<MoviePlace cpf={cpf} setCpf={setCpf}  nomeComprador={nomeComprador} setNomeComprador={setNomeComprador} idPlace={idPlace} setIdPlace={setIdPlace} />} />
+                <Route path="/sucesso" element={<Sucess />} />
             </Routes>
 
        </BrowserRouter>
--- src/MoviePlace.js
+++ src/MoviePlace.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams,useHistory} from 'react-router-dom';
 import imagem from "./assets/image9.png"
+import React from 'react';
 
 
-function PlacesChoices({name, isAvailable, id}){
+function PlacesChoices({name, isAvailable, id, setIdPlace, idPlace}){
+    function add() {
+        setSelected(true)
+        setIdPlace([...idPlace,id])
+    }
+
+
     const [selected, setSelected] = useState(false);
     const [available, setOccupied] = useState(isAvailable);
     
@@ -12,8 +19,8 @@ function PlacesChoices({name, isAvailable, id}){
     return (                    
         <div>
            {(available === false) && (selected === false) && (<div onClick={() => alert("Esse assento não está disponível")}><h4 className='place-occupied'>{name}</h4></div>)} 
-           {(available === true) && (selected === false) && (<div onClick={() => setSelected(true)}><h4 className='place-free'>{name}</h4></div>)}
-           {(selected === true) && (<div onClick={() => setSelected(false)}><h4 className='place-selected'>{name}</h4></div>)}
+           {(available === true) && (selected === false) && (<div onClick={() => add()}><h4 className='place-free'>{name}</h4></div>)}
+           {(selected === true) && (<div onClick={() => (setSelected(false)) }><h4 className='place-selected'>{name}</h4></div>)}
         </div>
     )
 }
@@ -32,13 +39,41 @@ function Footer({horario}) {
 
     )
 }
-export default function MoviePlace() {
+export default function MoviePlace({setUser, user, cpf, setCpf, setIdPlace, idPlace, nomeComprador, setNomeComprador}) {
     const { idSessao } = useParams();
     const [section, setSection] = useState([])
     const [places,setPlaces] = useState([]);
-    const[nomeComprador, setNomeComprador] = useState("");
-    const [cpf, setCpf] = useState("")
-    const [idPlace, setIdPlace] = useState([])
+    
+    function register2() {
+        alert("Preencha todos os Campos")
+    }
+
+    function register(event) {
+        if(idPlace.length == 0 || nomeComprador.length === 0 || cpf.length === 0){
+            alert("Preencha todos os campos!")
+        } else {
+            const body = {
+                ids: idPlace,
+                name: nomeComprador,
+                cpf: cpf
+            }
+    
+            const requisicao = axios.post("https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many", body)
+            requisicao
+                .then(res => {
+                    console.log(res.data)
+                   
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                
+            console.log(body)
+        }
+        <Link to={`/sucesso`} ></Link>
+        
+    }
+
 
     useEffect(() => {
         const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`);
@@ -53,12 +88,13 @@ export default function MoviePlace() {
     console.log(places);
     console.log(nomeComprador)
     console.log(cpf)
-    
+    console.log(idPlace)
+    console.log(user)
     return (
         <div className="initialPage">
             <h3> Selecione os assentos</h3>
             <div className='places-choices'>
-              {places.map(assento => <PlacesChoices id={assento.id} name={assento.name} isAvailable={assento.isAvailable}/>)}  
+              {places.map(assento => <PlacesChoices idPlace={idPlace} setIdPlace={setIdPlace} id={assento.id} name={assento.name} isAvailable={assento.isAvailable}/>)}  
                 
             </div>
             <div className='availability'>
@@ -76,16 +112,25 @@ export default function MoviePlace() {
                 </div>
             </div>
 
-        <div className='info'>
-            <h3>Nome do comprador:</h3>
-            <input placeholder='Digite seu nome..' value={nomeComprador} onChange={(e) => setNomeComprador(e.target.value)}/>
-            <h3>CPF do comprador:</h3>
-            <input placeholder='Digite seu nome..' value={cpf} onChange={(e) => setCpf(e.target.value)}/>
-        </div>
+            
+                <div className='info'>
+                    <h3>Nome do comprador:</h3>
+                    <input placeholder='Digite seu nome..' value={nomeComprador} onChange={(e) => setNomeComprador(e.target.value)} required/>
+                    <h3>CPF do comprador:</h3>
+                    <input placeholder='Digite seu nome..' value={cpf} onChange={(e) => setCpf(e.target.value)} required/>  
+                   {(idPlace.length == 0 || nomeComprador.length === 0 || cpf.length === 0) ?
+                    (<button onClick={register2} type='submit'> Reservar assentos </button>) : 
+                    <Link to={`/sucesso`} style={{ textDecoration: 'none' }} > <button onClick={register} type='submit'> Reservar assentos </button>
+                        </Link> }
+                    
+                    
+   
+                   
+                </div>
+      
 
-        <div className='book-seats'>
-            <button> Reservar assentos </button>
-        </div>
+            
+        
         
 
             <Footer horario={section.name} />
